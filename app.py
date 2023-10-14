47481c726ad1f40962270670425cfd222dbee2ab
vkodithala <varoon.kodithala@gmail.com>
2023-10-13 23:15:14 -0400

    testing functionality

--- app.py
+++ app.py
@@ -1,22 +1,22 @@
-import sys, json, os
+# import sys, json, os
 from flask import Flask
-from datetime import datetime
-from openai.embeddings_utils import get_embedding, cosine_similarity
-from pymongo.mongo_client import MongoClient
+# from datetime import datetime
+# from openai.embeddings_utils import get_embedding, cosine_similarity
+# from pymongo.mongo_client import MongoClient
 
 app = Flask(__name__)
 
-uri = "mongodb+srv://admin:@hackathon.otz1cym.mongodb.net/?retryWrites=true&w=majority"
-default_json = {
-        "dateTime": datetime.now(),
-        "phoneNo": "+16788973910",
-        "entry": "Hey! This is a test journal entry. I'm feeling really good today; I just fixed a huge bug in my app.",
-    }
+# uri = "mongodb+srv://admin:@hackathon.otz1cym.mongodb.net/?retryWrites=true&w=majority"
+# default_json = {
+#         "dateTime": datetime.now(),
+#         "phoneNo": "+16788973910",
+#         "entry": "Hey! This is a test journal entry. I'm feeling really good today; I just fixed a huge bug in my app.",
+#     }
 
 @app.route('/')
 def index():
-    journal_entry = default_json["entry"]
-    entry_embedding = get_embedding(journal_entry, engine="text-embedding-ada-002")
+    # journal_entry = default_json["entry"]
+    # entry_embedding = get_embedding(journal_entry, engine="text-embedding-ada-002")
     return "hello world 2"
 
 @app.route('/test')

4b8f56dbf8ecd3cbff343c1f042d92ebefe608cc
vkodithala <varoon.kodithala@gmail.com>
2023-10-14 08:50:14 -0400

    mongodb atlas config

--- app.py
+++ app.py
@@ -1,24 +1,96 @@
-# import sys, json, os
-from flask import Flask
-# from datetime import datetime
-# from openai.embeddings_utils import get_embedding, cosine_similarity
-# from pymongo.mongo_client import MongoClient
+from flask import Flask, request, jsonify
+from pymongo import MongoClient
+import json, sys
+import certifi
+from datetime import datetime, date
 
 app = Flask(__name__)
 
-# uri = "mongodb+srv://admin:@hackathon.otz1cym.mongodb.net/?retryWrites=true&w=majority"
-# default_json = {
-#         "dateTime": datetime.now(),
-#         "phoneNo": "+16788973910",
-#         "entry": "Hey! This is a test journal entry. I'm feeling really good today; I just fixed a huge bug in my app.",
+uri = "mongodb+srv://admin:Rewind1234!@hackathon.otz1cym.mongodb.net/?retryWrites=true&w=majority"
+client = MongoClient(uri, tlsCAFile=certifi.where())
+db = client["Rewind"]
+col = db["Test"]
+
+#pn is phone number
+@app.route("/getToday/<int:pn>", methods=['GET'])
+def get_today(pn):
+    today = str(datetime.now())
+    pattern = today[0:10]
+
+    query = {
+        "phoneNumber" : pn,
+        "dateTime": {"$regex": pattern}
+    }
+    cursor = col.find(query)
+    document_list = [doc for doc in cursor]
+    json_data = json.dumps(document_list, default=str, indent=2)
+    return json_data
+
+@app.route("/getAll/<int:pn>", methods=['GET'])
+def get_all(pn):
+    query = {
+        "phoneNumber": pn
+    }
+    cursor = col.find(query)
+    document_list = [doc for doc in cursor]
+    json_data = json.dumps(document_list, default=str, indent=2)
+    return json_data
+
+@app.route("/postEntry", methods=['POST'])
+def post_entry():
+    input_string = request.json['payload']
+    items = parse_payload(input_string)
+    fixed = {
+        "dateTime" : items[0],
+        "phoneNumber" : items[1],
+        "text" : items[2]
+    }
+    result = col.insert_one(fixed)
+    return f"Inserted document ID: {result.inserted_id}"
+
+# # def parse_payload(s: str) -> []:
+# #     ret = []
+# #     if(len(s) > 21):
+# #         unix_time = int(s[0:10])
+# #         normal_time = datetime.utcfromtimestamp(unix_time).strftime('%Y-%m-%d %H:%M:%S')
+# #         phone_no = int(s[10:21])
+# #         message = s[21:]
+# #         ret.append(normal_time)
+# #         ret.append(phone_no)
+# #         ret.append(message)
+# #     return ret
+
+# @app.route("/getMonth/<int:pn>", methods=['GET'])
+# def get_month(pn):
+#     today = str(datetime.now())
+#     pattern = today[0:10]
+
+#     query = {
+#         "phoneNumber" : pn,
+#         "dateTime": {"$regex": pattern}
 #     }
+#     cursor = col.find(query)
+#     document_list = [doc for doc in cursor]
+#     json_data = json.dumps(document_list, default=str, indent=2)
+#     return json_data
 
-@app.route('/')
-def index():
-    # journal_entry = default_json["entry"]
-    # entry_embedding = get_embedding(journal_entry, engine="text-embedding-ada-002")
-    return "hello world 2"
 
-@app.route('/test')
-def test():
-    return "text"
+# # @app.route("/getPast7/<int:pn>", methods=['GET'])
+# # def getPast7(pn):
+# #     today = str(datetime.now())
+# #     pattern = today[0:10]
+# #     currDay = int(pattern[-2:])
+# #     longString = ""
+# #     for i in range(currDay - 6, currDay + 1):
+# #         currPatt = today[0:8] + str(i)
+# #         if (i < (currDay)):
+# #             currPatt += "|"
+# #         longString += currPatt
+# #     query = {
+# #         "phoneNumber" : pn,
+# #         "dateTime": {"$regex": longString}
+# #     }
+# #     cursor = col.find(query)
+# #     document_list = [doc for doc in cursor]
+# #     json_data = json.dumps(document_list, default=str, indent=2)
+# #     return json_data
